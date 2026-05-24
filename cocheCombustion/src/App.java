14fff582b05bf0b493afcca324e4a7673e216b30
DanielRiveroP <riveroperezmesa@gmail.com>
2026-05-24 13:48:36 +0100

    movimiento del coche sin encender

--- cocheCombustion/src/App.java
+++ cocheCombustion/src/App.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+import net.salesianos.cocheCombustion.CocheCombustion;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        String color = "Rojo";
+        int numPuertas = 4;
+        int numRuedas = 4;
+        String modelo = "Sedan";
+        String matricula = "1234ABC";
+        int litrosCombustible = 20;
+        CocheCombustion coche = new CocheCombustion(color, numPuertas, numRuedas, modelo, matricula, litrosCombustible) {
+        };
+
+        coche.mover();
+
+        coche.arrancar();
+        for (int i = 0; i < 3; i++) {
+            coche.mover();
+        }
+
+        
+
+        sc.close();
     }
 }
---
+++ cocheCombustion/src/net/salesianos/cocheCombustion/CocheCombustion.java
@@ -0,0 +1,58 @@
+package net.salesianos.cocheCombustion;
+
+import net.salesianos.vehiculos.Vehiculos;
+
+public abstract class CocheCombustion extends Vehiculos {
+
+    protected boolean motorEncendido = false;
+
+    protected int litrosCombustible;
+
+    public CocheCombustion(String color, int numPuertas, int numRuedas, String modelo, String matricula, int litrosCombustible) {
+        super(color, numPuertas, numRuedas, modelo, matricula);
+        this.litrosCombustible = litrosCombustible;
+    }
+
+    public boolean isMotorEncendido() {
+        return motorEncendido;
+    }
+
+    public void setMotorEncendido(boolean motorEncendido) {
+        this.motorEncendido = motorEncendido;
+    }
+
+    public int getLitrosCombustible() {
+        return litrosCombustible;
+    }
+
+    public void setLitrosCombustible(int litrosCombustible) {
+        this.litrosCombustible = litrosCombustible;
+    } 
+
+    @Override
+    public void arrancar() {
+        if (motorEncendido = true) {
+            litrosCombustible -= 1;
+            System.out.println("El coche ha arrancado. Litros de combustible restantes: " + litrosCombustible);
+        } else if (litrosCombustible <= 0) {
+            System.out.println("ENo hay combustible por lo tanto no se puede arrancar el coche.");
+        }
+    }
+
+    @Override
+    public void pararMotor() {
+        motorEncendido = false;
+        System.out.println("El motor se ha apagado.");
+    }
+
+    @Override
+    public void mover() {
+        if (motorEncendido == true) {
+            litrosCombustible -= 1;
+            System.out.println("El coche se ha movido. Litros de combustible restantes: " + litrosCombustible);
+        } else if (litrosCombustible <= 0 || motorEncendido == false) {
+            System.out.println("No se puede mover el coche porque el motor está apagado o no hay combustible.");
+
+        }
+    }
+}
